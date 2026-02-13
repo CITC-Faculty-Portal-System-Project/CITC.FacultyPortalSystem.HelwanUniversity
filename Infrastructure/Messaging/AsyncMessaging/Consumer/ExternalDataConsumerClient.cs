088e5b92f2350b6f93e8d25201301e32c42b90a9
Salah Alkhatib <salahalkhatib004@gmail.com>
2026-02-13 02:08:17 +0200

    UpdatingRabbitMQ + Adding DLQS

--- Infrastructure/Messaging/AsyncMessaging/Consumer/ExternalDataConsumerClient.cs
+++ Infrastructure/Messaging/AsyncMessaging/Consumer/ExternalDataConsumerClient.cs
@@ -1,283 +1,340 @@
 ﻿using Messaging.AsyncMessaging.Consumer.Helpers;
+using Messaging.AsyncMessaging.Settings;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using Services.Abstraction.Contracts;
+using System.Collections.Concurrent;
 using System.Text;
 
 namespace Messaging.AsyncMessaging.Consumer
 {
-	/*public class ExternalDataConsumerClient : BackgroundService
-    {
-
-        private IConnection _connection;
-        private IModel _channel;
-        private readonly IServiceScopeFactory _scopeFactory;
-
-        public ExternalDataConsumerClient(IConfiguration config, IServiceScopeFactory scopeFactory) 
-        {
-
-            var factory = new ConnectionFactory
-            {
-                HostName = config["RabbitMQ:Host"] ?? "localhost",
-                Port = int.Parse(config["RabbitMQ:Port"] ?? "5672"),
-                UserName = config["RabbitMQ:Username"] ?? "guest",
-                Password = config["RabbitMQ:Password"] ?? "guest"
-            };
-            _scopeFactory = scopeFactory;
-            _connection = factory.CreateConnection();
-            _channel = _connection.CreateModel();
-            SetupRabbitMq();
-        }
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            StartConsumers(stoppingToken);
-            return Task.CompletedTask;
-        }
-
-        private void SetupRabbitMq()
-        {
-            // 1. Exchange
-            _channel.ExchangeDeclare(
-                exchange: "data_exchange",
-                type: ExchangeType.Direct,
-                durable: true);
-
-            // 2. Declare queues
-            Declare("AcademicQualification-queue");
-            Declare("employmentDegree-queue");
-            Declare("ManagerialPosition-queue");
-
-            // 3. Bind queues to routing keys
-            _channel.QueueBind("AcademicQualification-queue", "data_exchange", "academicQualificationsRK");
-            _channel.QueueBind("employmentDegree-queue", "data_exchange", "employmentDegreeRK");
-            _channel.QueueBind("ManagerialPosition-queue", "data_exchange", "managerialPositionsRK");
-        }
-        private void Declare(string queueName)
-        {
-            _channel.QueueDeclare(
-                queue: queueName,
-                durable: true,
-                exclusive: false,
-                autoDelete: false);
-        }
-
-        private void StartConsumers(CancellationToken stoppingToken)
-        {
-            StartConsumer("AcademicQualification-queue");
-            StartConsumer("employmentDegree-queue");
-            StartConsumer("ManagerialPosition-queue");
-        }
-
-        private void StartConsumer(string queueName)
-        {
-            var consumer = new EventingBasicConsumer(_channel);
-
-            consumer.Received += async (sender, ea) =>
-            {
-
-                string msg = Encoding.UTF8.GetString(ea.Body.ToArray());
-                string routing = ea.RoutingKey;
-
-                try
-                {
-                    using var scope = _scopeFactory.CreateScope();
-                    var service = scope.ServiceProvider.GetRequiredService<IExternalDataHandlingService>();
-
-                    switch (routing)
-                    {
-                        case "academicQualificationsRK":
-                            var dto = await service.AcademicDataHandle(msg);
-                            break;
-
-                        case "employmentDegreeRK":
-                            await service.EmploymentDataHandle(msg);
-                            break;
-
-                        case "managerialPositionsRK":
-                            await service.ManagerialDataHandle(msg);
-                            break;
-                    }
-
-                    _channel.BasicAck(ea.DeliveryTag, false);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error: {ex.Message}");
-                    _channel.BasicNack(ea.DeliveryTag, false, requeue: true);
-                }
-            };
-
-            _channel.BasicConsume(queueName, autoAck: false, consumer);
-        }
-
-        public override void Dispose()
-        {
-            _channel?.Close();
-            _connection?.Close();
-            base.Dispose();
-        }
-    }*/
-
 	public class ExternalDataConsumerClient : BackgroundService
     {
         private readonly IServiceScopeFactory _scopeFactory;
         private readonly IRabbitMQConnection _connection;
-        private List<IModel> _channels = new();
+		private readonly RabbitMQSettings _settings;
+		private readonly ConcurrentBag<IChannel> _channels = new();
 
-
-        public ExternalDataConsumerClient(IRabbitMQConnection rabbitMQConnection, IServiceScopeFactory scopeFactory)
+        public ExternalDataConsumerClient(
+            IRabbitMQConnection rabbitMQConnection, 
+            IServiceScopeFactory scopeFactory,
+			IOptions<RabbitMQSettings> options)
         {
 			_connection = rabbitMQConnection;
 			_scopeFactory = scopeFactory;
+			_settings = options.Value;
 
-        }
+		}
 
-        public override Task StartAsync(CancellationToken cancellationToken)
+        public override async Task StartAsync(CancellationToken cancellationToken)
         {
             try
             {
-                InitializeRabbitMQ();
-                return base.StartAsync(cancellationToken);
+                await InitializeRabbitMQAsync(cancellationToken);
+                await base.StartAsync(cancellationToken);
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"==> Error during RabbitMQ Initialization: {ex.Message}");
-				throw;
 			}
         }
 
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            try
-            {
-                StartConsumers(stoppingToken);
-                return Task.CompletedTask;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"==> Error during starting consumers: {ex.Message}");
-                throw;
-            }
-        }
-
-		public override void Dispose()
+		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 		{
-			Console.WriteLine("--> Disposing consumer channels...");
+			try
+			{ 
+					await StartConsumersAsync(stoppingToken);
+					await Task.Delay(Timeout.Infinite, stoppingToken);
+			}
+			catch (OperationCanceledException)
+			{
+				//> Expected during shutdown, no action needed.
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"==> Error during starting consumers: {ex.Message}");
+				throw;
+			}
+		}
 
-			foreach (var channel in _channels)
+		public override async Task StopAsync(CancellationToken cancellationToken)
+		{
+			if (_channels.Any())
 			{
 				try
 				{
-					if (channel.IsOpen)
-						channel.Close();
-					channel.Dispose();
+					foreach (var channel in _channels)
+					{ 
+						if (channel.IsOpen) 
+							await channel.CloseAsync(cancellationToken: cancellationToken); 
+					}
 				}
 				catch (Exception ex)
 				{
-					Console.WriteLine($"--> Failed to dispose channel: {ex.Message}");
+					Console.WriteLine($"--> Failed to close channels: {ex.Message}");
 				}
 			}
+		}
 
-			Console.WriteLine("--> Consumer disposed (connection kept alive).");
+		public override void Dispose()
+		{
+			if (_channels.Any())
+			{
+				Console.WriteLine("--> Disposing consumer channels...");
 
+				foreach (var channel in _channels)
+				{
+					try
+					{
+						channel.Dispose();
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine($"--> Failed to dispose channel: {ex.Message}");
+					}
+				}
+				Console.WriteLine("--> Consumer disposed (connection kept alive).");
+			}
 			base.Dispose();
 		}
 
 
 		#region Helpers
-		private void InitializeRabbitMQ()
+		private async Task InitializeRabbitMQAsync(CancellationToken cancellationToken)
         {
-			using var setupChannel = _connection.GetConnection().CreateModel();
-			setupChannel.ExchangeDeclare(RabbitMQConstants.ExchangeName, ExchangeType.Direct, durable: true);
+			using var setupChannel = await _connection.GetConnection().CreateChannelAsync(cancellationToken: cancellationToken);
+
+            await setupChannel.ExchangeDeclareAsync(
+                RabbitMQConstants.ExchangeName, 
+                ExchangeType.Direct, 
+                durable: true,
+                cancellationToken: cancellationToken);
+
+            #region DL Exchange/Prop
+
+            await setupChannel.ExchangeDeclareAsync(
+                _settings.ExternalDataDLX,
+                ExchangeType.Direct,
+                durable: true,
+                autoDelete: false,
+                cancellationToken: cancellationToken);
+
+			var args = new Dictionary<string, object>
+			{
+				{ "x-dead-letter-exchange", _settings.ExternalDataDLX},
+				{ "x-dead-letter-routing-key", _settings.ExternalDataDLRK}
+			};
+
+			#endregion
 
 			var queues = QueueInitializer.InitializeQueues();
 
 			foreach (var queue in queues)
 			{
-				setupChannel.QueueDeclare(queue.QueueName, durable: true, exclusive: false, autoDelete: false);
-				setupChannel.QueueBind(queue.QueueName, RabbitMQConstants.ExchangeName, queue.RoutingKey);
-			}
-		}
+				await setupChannel.QueueDeclareAsync(
+                    queue.QueueName,
+                    durable: true,
+                    exclusive: false, 
+                    autoDelete: false,
+                    arguments: args!,
+					cancellationToken: cancellationToken);
 
-        private void StartConsumers(CancellationToken cancellationToken)
+				await setupChannel.QueueBindAsync(
+                    queue.QueueName, 
+                    RabbitMQConstants.ExchangeName, 
+                    queue.RoutingKey,
+                    cancellationToken: cancellationToken);
+			}
+
+            #region DLQ/Bind
+
+            await setupChannel.QueueDeclareAsync(
+                _settings.ExternalDataDLQ,
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null,
+                cancellationToken: cancellationToken);
+
+			await setupChannel.QueueBindAsync(
+				_settings.ExternalDataDLQ,
+				_settings.ExternalDataDLX,
+				_settings.ExternalDataDLRK,
+				cancellationToken: cancellationToken);
+
+			#endregion
+
+		}
+        private async Task StartConsumersAsync(CancellationToken cancellationToken)
         {
             var queues = QueueInitializer.InitializeQueues();
 
             foreach(var queue in queues)
             {
-				var channel = _connection!.GetConnection().CreateModel();
+				var channel = await _connection!.GetConnection().CreateChannelAsync(cancellationToken: cancellationToken);
 				_channels.Add(channel);
-				StartConsumer(channel, queue.QueueName,queue.RoutingKey);
+				await StartConsumerAsync(channel, queue.QueueName,queue.RoutingKey, cancellationToken);
 			}
 		}
-
-        private void StartConsumer(IModel channel, string queueName, string routingKey)
+        private async Task StartConsumerAsync(IChannel channel, string queueName, string routingKey, CancellationToken cancellationToken)
         {
-            var consumer = new AsyncEventingBasicConsumer(channel);
+			if (channel is null || !channel.IsOpen)
+			{
+				Console.WriteLine("--> Channel is not available for processing.");
+				return;
+			}
 
-			consumer.Received += async (sender, ea) =>
+			await channel.BasicQosAsync(0, prefetchCount: 30, global: false);
+
+			var consumer = new AsyncEventingBasicConsumer(channel);
+
+			consumer.ReceivedAsync += async (sender, ea) =>
 			{
 				var message = Encoding.UTF8.GetString(ea.Body.ToArray());
 				try
 				{
-					await HandleMessageAsync(ea.RoutingKey, message);
-					channel.BasicAck(ea.DeliveryTag, multiple: false);
+					await HandleMessageAsync(ea, message, channel, cancellationToken);
+					await channel.BasicAckAsync(ea.DeliveryTag, multiple: false);
 				}
 				catch (Exception ex)
 				{
 					Console.WriteLine($"==> Error processing message from '{queueName}': {ex.Message}");
-					channel.BasicNack(ea.DeliveryTag, multiple: false, requeue: true);
+					await channel.BasicNackAsync(ea.DeliveryTag, multiple: false, requeue: true);
 				}
 			};
 
-			channel.BasicConsume(queue: queueName, autoAck: false, consumer);
+			await channel.BasicConsumeAsync(queue: queueName, autoAck: false, consumer);
 		}
-
-        //Can be later moved into a separate Class
-		private async Task HandleMessageAsync(string routingKey, string message)
-		{
-			using var scope = _scopeFactory.CreateScope();
-			var service = scope.ServiceProvider.GetRequiredService<IExternalDataHandlingService>();
-
-			switch (routingKey)
+        private async Task HandleMessageAsync(BasicDeliverEventArgs ea, string message, IChannel channel, CancellationToken cancellationToken)
+        {
+			if(cancellationToken.IsCancellationRequested)
 			{
-				case RabbitMQConstants.AcademicQualificationRoutingKey:
-					await service.AcademicDataHandle(message);
-					break;
-				case RabbitMQConstants.EmploymentDegreeRoutingKey:
-					await service.EmploymentDataHandle(message);
-					break;
-				case RabbitMQConstants.ManagerialPositionRoutingKey:
-				    await service.ManagerialDataHandle(message);
-					break;
-				case RabbitMQConstants.ContactDataRoutingKey:
-				    await service.ContactDataHandle(message);
-					break;
-				case RabbitMQConstants.PersonalDataRoutingKey:
-					await service.PersonalDataHandle(message);
-					break;
-				/*case RabbitMQConstants.SpecializationRoutingKey:
-					//await service.SpecializationDataHandle(message);
-					break;*/
-				
-                case RabbitMQConstants.ScientificDutyRoutingKey:
-					await service.ScientificDutyDataHandle(message);
-					break;
-				case RabbitMQConstants.TrainingProgramRoutingKey:
-					await service.TrainingProgramDataHandle(message);
-					break;
-				case RabbitMQConstants.ThesisSupervisionRoutingKey:
-					await service.ThesisSupervisingDataHandle(message);
-					break;
-				case RabbitMQConstants.ThesisDataRoutingKey:
-					await service.ThesisDataHandle(message);
-					break;
-				default:
-					throw new InvalidOperationException($"Unknown routing key: {routingKey}");
+				//might nack
+				Console.WriteLine("--> Cancellation requested before processing message.");
+				return;
 			}
+			try
+			{
+				using var scope = _scopeFactory.CreateScope();
+				var service = scope.ServiceProvider.GetRequiredService<IExternalDataHandlingService>();
+
+				switch (ea.RoutingKey)
+				{
+					case RabbitMQConstants.AcademicQualificationRoutingKey:
+						throw new FormatException("Simulated format error for testing DLQ.");
+						//await service.AcademicDataHandle(message);
+						//break;
+					case RabbitMQConstants.EmploymentDegreeRoutingKey:
+						await service.EmploymentDataHandle(message);
+						break;
+					case RabbitMQConstants.ManagerialPositionRoutingKey:
+						await service.ManagerialDataHandle(message);
+						break;
+					case RabbitMQConstants.ContactDataRoutingKey:
+						await service.ContactDataHandle(message);
+						break;
+					case RabbitMQConstants.PersonalDataRoutingKey:
+						throw new FormatException("Simulated format error for testing DLQ.");
+						//await service.PersonalDataHandle(message);
+						//break;
+					case RabbitMQConstants.ScientificDutyRoutingKey:
+						await service.ScientificDutyDataHandle(message);
+						break;
+					case RabbitMQConstants.TrainingProgramRoutingKey:
+						await service.TrainingProgramDataHandle(message);
+						break;
+					case RabbitMQConstants.ThesisSupervisionRoutingKey:
+						throw new FormatException("Simulated format error for testing DLQ.");
+						//await service.ThesisSupervisingDataHandle(message);
+						//break;
+					case RabbitMQConstants.ThesisDataRoutingKey:
+						throw new FormatException("Simulated format error for testing DLQ.");
+						//await service.ThesisDataHandle(message);
+						//break;
+					default:
+						throw new InvalidOperationException($"Unknown routing key: {ea.RoutingKey}");
+				}
+			}
+			catch (InvalidOperationException ex)
+			{
+				Console.WriteLine($"==> Routing error: {ex.Message}");
+			}
+			catch (FormatException)
+			{
+				await channel.BasicRejectAsync(ea.DeliveryTag, requeue: false);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"--> Processing failed: {ex.Message}");
+
+				var retryCount = GetRetryCount(ea.BasicProperties.Headers);
+
+				if (retryCount < _settings.MaxRetryCount)
+				{
+					await RepublishWithRetryAsync(ea, retryCount + 1, cancellationToken);
+					await channel.BasicAckAsync(ea.DeliveryTag, false);
+					Console.WriteLine($"Re-Published for message {message}");
+				}
+				else
+				{
+					Console.WriteLine($"--> Max retries exceeded → DLQ [Message : {message}]");
+					await channel.BasicRejectAsync(ea.DeliveryTag, requeue: false);
+				}
+			}
+
+        }
+
+		#region DLQ Helpers
+		private int GetRetryCount(IDictionary<string, object?>? headers)
+		{
+			if (headers == null || !headers.TryGetValue("data-retry-count", out var value))
+				return 0;
+
+			return value switch
+			{
+				byte[] bytes => int.Parse(Encoding.UTF8.GetString(bytes)),
+				int i => i,
+				_ => 0
+			};
 		}
+
+		private async Task RepublishWithRetryAsync(BasicDeliverEventArgs ea, int retryCount, CancellationToken cancellationToken)
+		{
+			using var retryChannel = await _connection.GetConnection().CreateChannelAsync(cancellationToken: cancellationToken);
+
+			if (retryChannel == null || !retryChannel.IsOpen)
+			{
+				Console.WriteLine("--> Failed to create retry channel.");
+				return;
+			}
+
+			var props = new BasicProperties
+			{
+				Persistent = true,
+				Headers = new Dictionary<string, object?>
+				{
+					{ "data-retry-count", retryCount }
+				}
+			};
+
+			await retryChannel.BasicPublishAsync(
+				exchange: ea.Exchange,
+				routingKey: ea.RoutingKey,
+				mandatory: false,
+				basicProperties: props,
+				body: ea.Body);
+
+
+
+			Console.WriteLine($"--> Retry {retryCount}/{_settings.MaxRetryCount}");
+
+		}
+
+		#endregion
+
 		#endregion
 
 	}
--- Infrastructure/Messaging/AsyncMessaging/IRabbitMQConnection.cs
+++ Infrastructure/Messaging/AsyncMessaging/IRabbitMQConnection.cs
@@ -1,13 +1,8 @@
 ﻿using RabbitMQ.Client;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Messaging.AsyncMessaging
 {
-    public interface IRabbitMQConnection
+	public interface IRabbitMQConnection
     {
         public IConnection GetConnection();
     }
