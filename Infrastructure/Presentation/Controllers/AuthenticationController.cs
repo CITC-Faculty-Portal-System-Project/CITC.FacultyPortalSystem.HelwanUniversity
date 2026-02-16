3f1139e083ef892ce38edbc6ed205c5b96024abf
Ahmed Ehab <135270188+AhmedEhab5006@users.noreply.github.com>
2026-02-16 02:16:35 +0200

    LogOut Added

--- Infrastructure/Presentation/Controllers/AuthenticationController.cs
+++ Infrastructure/Presentation/Controllers/AuthenticationController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Services.Abstraction.Contracts;
 using Shared.Dtos.Auth;
 using Shared.Dtos.IdentityModule;
@@ -70,6 +73,20 @@ namespace Presentation.Controllers
             var email = User.FindFirstValue(ClaimTypes.Email);
             var user = await _serviceManager.AuthenticationService.GetCurrentUserAsync(email ?? "");
             return Ok(user);
+        }
+
+        [HttpPost("Logout")]
+        [Authorize]
+        public IActionResult Logout()
+        {
+            Response.Cookies.Delete("jwtToken", new CookieOptions
+            {
+                Path = "/",
+                Secure = true,
+                SameSite = SameSiteMode.None
+            });
+
+            return Ok("Logged out");
         }
     }
 }
