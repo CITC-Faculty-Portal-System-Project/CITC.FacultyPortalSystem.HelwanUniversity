06bb7f97c89ca105c254db5c65f2cff601253fc5
Ahmed Ehab <135270188+AhmedEhab5006@users.noreply.github.com>
2026-03-16 04:42:11 +0200

    Forgot to use signalR in Contoller so I Used it

--- Infrastructure/Presentation/Controllers/MessagingAndChattingModule/MessagingController.cs
+++ Infrastructure/Presentation/Controllers/MessagingAndChattingModule/MessagingController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 using Shared;
 using Shared.Dtos.MessagingAndChattingModule;
 using Shared.SpecificationParameters.MessagingAndChattingModule;
@@ -6,36 +7,66 @@ using Shared.SpecificationParameters.MessagingAndChattingModule;
 namespace Presentation.Controllers.MessagingAndChattingModule
 {
     [Authorize]
-    public class MessagingController(IServiceManager _serviceManager) : ApiController
+    public class MessagingController(IServiceManager _serviceManager , IHubContext _hubContext) : ApiController
     {
         #region Chat
 
         [ProducesResponseType(typeof(MessageResponseDTO), StatusCodes.Status200OK)]
         [HttpPost("Message")]
         public async Task<ActionResult<MessageResponseDTO>> SendMessage(MessageSendDTO request)
-            => Ok(await _serviceManager.ChatService.SendMessageAsync(request));
+        {
+            var result = await _serviceManager.ChatService.SendMessageAsync(request);
+
+            await _hubContext.Clients.Group(request.ConversationId.ToString())
+               .SendAsync("ConversationUpdated", result);
+
+            await _hubContext.Clients.User(request.RecieverId.ToString())
+                .SendAsync("ReceiveMessage", result);
+
+            return Ok(result);
+        }
 
         [ProducesResponseType(typeof(CursorPaginatedResult<MessageResponseDTO, long>), StatusCodes.Status200OK)]
         [HttpGet("Conversation/{conversationId}")]
         public async Task<ActionResult<CursorPaginatedResult<MessageResponseDTO, long>>> GetConversationMessages(
             int conversationId, [FromQuery] long? cursor, [FromQuery] int take = 20)
-            => Ok(await _serviceManager.ChatService.GetConversationMessagesAsync(
+        {
+           var result = await _serviceManager.ChatService.GetConversationMessagesAsync(
                 new MessageSpecificationParameters
                 {
                     ConversationId = conversationId,
                     BeforeMessageId = cursor,
                     Take = take
-                }));
+                });
+
+            return Ok(result);
+
+        }
 
         [ProducesResponseType(typeof(MessageResponseDTO), StatusCodes.Status200OK)]
         [HttpPut("Message/{messageId}/Delivered")]
         public async Task<ActionResult<MessageResponseDTO>> MarkMessageAsDelivered(long messageId)
-            => Ok(await _serviceManager.ChatService.MarkMessageAsDeliveredAsync(messageId));
+        {
+            var result = await _serviceManager.ChatService.MarkMessageAsDeliveredAsync(messageId);
+
+            await _hubContext.Clients.Group(result.ConversationId.ToString())
+               .SendAsync("MessageDelivered", result);
+
+            return Ok(result);
+        }
 
         [ProducesResponseType(typeof(MessageResponseDTO), StatusCodes.Status200OK)]
         [HttpPut("Message/{messageId}/Read")]
         public async Task<ActionResult<MessageResponseDTO>> MarkMessageAsRead(long messageId)
-            => Ok(await _serviceManager.ChatService.MarkMessageAsReadAsync(messageId));
+        {
+           var result = await _serviceManager.ChatService.MarkMessageAsReadAsync(messageId);
+
+            await _hubContext.Clients.Group(result.ConversationId.ToString())
+               .SendAsync("MessageRead", result);
+
+            return Ok(result);
+
+        }
 
         #endregion
 
