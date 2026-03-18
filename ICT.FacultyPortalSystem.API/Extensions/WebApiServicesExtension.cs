34e617db0105e69b9b850c1ec0b1552a56721fe8
Ahmed Ehab <135270188+AhmedEhab5006@users.noreply.github.com>
2026-03-18 03:45:08 +0200

    Updating Cors for deployment

--- ICT.FacultyPortalSystem.API/Extensions/WebApiServicesExtension.cs
+++ ICT.FacultyPortalSystem.API/Extensions/WebApiServicesExtension.cs
@@ -28,7 +28,7 @@ namespace ICIT.FacultyPortalSystem.API.Extensions
                         "http://localhost",
                         "http://127.0.0.1",
                         "http://localhost:80",
-                        "http://127.0.0.1:80"
+                        "http://127.0.0.1:3000"
                         )
                          .AllowCredentials();
                 });
