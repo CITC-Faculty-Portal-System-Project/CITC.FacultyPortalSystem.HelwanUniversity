59ec9dcdd8360ca7426c7b4f1bff7d6d27ec71a1
Ahmed Ehab <135270188+AhmedEhab5006@users.noreply.github.com>
2026-04-28 00:55:15 +0300

    fix department mapping

--- Services/MappingProfiles/FacultyMemberDataProfile.cs
+++ Services/MappingProfiles/FacultyMemberDataProfile.cs
@@ -44,6 +44,7 @@ namespace Services.MappingProfiles
             CreateMap<PersonalData, ProfileDashboardResponseDTO>()
                 .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
                 .ForMember(dest => dest.University, opt => opt.MapFrom(src => src.University))
+                .ForMember(dest => dest.Department, opt => opt.Ignore())
                 .ForMember(dest => dest.NameAr, opt => opt.MapFrom(src => src.NameAr))
                 .ForMember(dest => dest.NameEn, opt => opt.MapFrom(src => src.NameAr))
                 .ForMember(dest => dest.BioSummary, opt => opt.MapFrom(src => src.BioSummary))
