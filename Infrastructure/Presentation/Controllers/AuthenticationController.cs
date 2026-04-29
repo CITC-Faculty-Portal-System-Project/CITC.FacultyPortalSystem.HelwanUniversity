0cd6693fcac00518cc11af024de8f2b843f61678
Ahmed Ehab <135270188+AhmedEhab5006@users.noreply.github.com>
2026-04-29 17:49:05 +0300

    ss

--- Infrastructure/Presentation/Controllers/AuthenticationController.cs
+++ Infrastructure/Presentation/Controllers/AuthenticationController.cs
@@ -32,7 +32,7 @@ namespace Presentation.Controllers
             Response.Cookies.Append("jwtToken", result.Token, cookieOptions);
             var frontendResponse = new LoginClaimsResponseDto
             {
-                Email = result.Email,
+                //Email = result.Email,
                 UserName = result.UserName,
                 Roles = result.Roles,
                 //NationalNumber = result.NationalNumber,
