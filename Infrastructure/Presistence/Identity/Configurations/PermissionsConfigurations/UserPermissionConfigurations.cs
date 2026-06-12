cc836dd129929ce1dc8fc7c2086b47d0f2cbda3d
Ahmed Ehab <135270188+AhmedEhab5006@users.noreply.github.com>
2026-06-12 23:33:45 +0300

    sa

--- Infrastructure/Presistence/Identity/Configurations/PermissionsConfigurations/UserPermissionConfigurations.cs
+++ Infrastructure/Presistence/Identity/Configurations/PermissionsConfigurations/UserPermissionConfigurations.cs
@@ -30,53 +30,53 @@ namespace Presistence.Identity.Configurations.PermissionsConfigurations
 
             #endregion
 
-            #region DataSeeding
+            //#region DataSeeding
 
-            var userId = new Guid("A9923638-8866-4A89-A9FE-9CF329CFC8F7");
-            var seedDate = new DateTime(2025, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            var userPermissions = new List<UserPermission>();
-            var permissionId = 1;
+            //var userId = new Guid("A9923638-8866-4A89-A9FE-9CF329CFC8F7");
+            //var seedDate = new DateTime(2025, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            //var userPermissions = new List<UserPermission>();
+            //var permissionId = 1;
 
-            foreach (PermissionType module in Enum.GetValues(typeof(PermissionType)))
-            {
-                for (int i = 0; i < 4; i++)
-                {
-                    userPermissions.Add(new UserPermission
-                    {
-                        UserId = userId,
-                        PermissionId = permissionId++,
-                        AssignedBy = "System",
-                        AssignedAt = seedDate,
-                        AssignerId = Guid.Empty,
-                        CreatedBy = "System",
-                        CreatedAt = seedDate,
-                        IsDeleted = false,
-                        VersionNo = 1
-                    });
-                }
-                if (module == PermissionType.Tickets)
-                {
-                    for (int i = 0; i < 8; i++)
-                    {
-                        userPermissions.Add(new UserPermission
-                        {
-                            UserId = userId,
-                            PermissionId = permissionId++,
-                            AssignedBy = "System",
-                            AssignedAt = seedDate,
-                            AssignerId = Guid.Empty,
-                            CreatedBy = "System",
-                            CreatedAt = seedDate,
-                            IsDeleted = false,
-                            VersionNo = 1
-                        });
-                    }
-                }
-            }
+            //foreach (PermissionType module in Enum.GetValues(typeof(PermissionType)))
+            //{
+            //    for (int i = 0; i < 4; i++)
+            //    {
+            //        userPermissions.Add(new UserPermission
+            //        {
+            //            UserId = userId,
+            //            PermissionId = permissionId++,
+            //            AssignedBy = "System",
+            //            AssignedAt = seedDate,
+            //            AssignerId = Guid.Empty,
+            //            CreatedBy = "System",
+            //            CreatedAt = seedDate,
+            //            IsDeleted = false,
+            //            VersionNo = 1
+            //        });
+            //    }
+            //    if (module == PermissionType.Tickets)
+            //    {
+            //        for (int i = 0; i < 8; i++)
+            //        {
+            //            userPermissions.Add(new UserPermission
+            //            {
+            //                UserId = userId,
+            //                PermissionId = permissionId++,
+            //                AssignedBy = "System",
+            //                AssignedAt = seedDate,
+            //                AssignerId = Guid.Empty,
+            //                CreatedBy = "System",
+            //                CreatedAt = seedDate,
+            //                IsDeleted = false,
+            //                VersionNo = 1
+            //            });
+            //        }
+            //    }
+            //}
 
-            builder.HasData(userPermissions);
+            //builder.HasData(userPermissions);
 
-            #endregion
+            //#endregion
 
 
         }
